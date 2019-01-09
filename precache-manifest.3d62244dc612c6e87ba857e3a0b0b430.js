09e9f08b772ada28ff8a8cfcc0f1f488d6b7d129
Timothyhs <timothy.hsu@citrix.com>
2019-01-08 16:15:57 -0800

    Updates

---
+++ precache-manifest.3d62244dc612c6e87ba857e3a0b0b430.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "76f991ee8c47a71feffd8113535e819e",
+    "url": "/purk-web/static/media/lightbulb-border.76f991ee.png"
+  },
+  {
+    "revision": "8a91bfbe26d67a9338bd",
+    "url": "/purk-web/static/css/main.85858713.chunk.css"
+  },
+  {
+    "revision": "79e201d8fba03d2a25d57e009b1e2481",
+    "url": "/purk-web/static/media/apple_keyboard_mouse.79e201d8.jpg"
+  },
+  {
+    "revision": "538d098b1654ec0295db",
+    "url": "/purk-web/static/js/1.538d098b.chunk.js"
+  },
+  {
+    "revision": "015a6af48446e2b3b611",
+    "url": "/purk-web/static/js/runtime~main.015a6af4.js"
+  },
+  {
+    "revision": "17f98377ebda86dfb6aa49c402608b89",
+    "url": "/purk-web/static/media/PurkLogoSalmon.17f98377.png"
+  },
+  {
+    "revision": "5eea846df509b1f6ed69042818763b4a",
+    "url": "/purk-web/static/media/timer-mix.5eea846d.png"
+  },
+  {
+    "revision": "4137c4850ca5b6f932d513fa5482cd66",
+    "url": "/purk-web/static/media/money-coins.4137c485.png"
+  },
+  {
+    "revision": "5eaa63f544a7fcd381b1aeebd659e054",
+    "url": "/purk-web/static/media/car-gas.5eaa63f5.png"
+  },
+  {
+    "revision": "8a91bfbe26d67a9338bd",
+    "url": "/purk-web/static/js/main.8a91bfbe.chunk.js"
+  },
+  {
+    "revision": "9797f76416ce5f7f8ecd9251dfa6931c",
+    "url": "/purk-web/static/media/park-border.9797f764.png"
+  },
+  {
+    "revision": "326ed97d579283a0e1a2a415a67c7489",
+    "url": "/purk-web/static/media/internet.326ed97d.png"
+  },
+  {
+    "revision": "2fc665f3118f89e3eb1cbc9c1639a0bb",
+    "url": "/purk-web/static/media/Questrial-Regular.2fc665f3.ttf"
+  },
+  {
+    "revision": "99e6f15dece8b6cbc0edee1da31e0365",
+    "url": "/purk-web/static/media/Proxima Nova Reg.99e6f15d.ttf"
+  },
+  {
+    "revision": "a046e3a692d1f09ba7d30016b64df4ef",
+    "url": "/purk-web/static/media/City.a046e3a6.png"
+  },
+  {
+    "revision": "6720d44ce82c265318b7b8dbb9c6c583",
+    "url": "/purk-web/static/media/roadtrip.6720d44c.jpg"
+  },
+  {
+    "revision": "468cafd1e66cad6f60033cbf1f413664",
+    "url": "/purk-web/static/media/parking_spot.468cafd1.jpg"
+  },
+  {
+    "revision": "538d098b1654ec0295db",
+    "url": "/purk-web/static/css/1.79b1262e.chunk.css"
+  },
+  {
+    "revision": "7c6aa98772d39efaf89315b74b659c3e",
+    "url": "/purk-web/index.html"
+  }
+];
